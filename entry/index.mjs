18d6e60159b638f783fd4143a7bebd084e6abddf
오병준 <mail@luftaquila.io>
2025-04-04 02:28:00 +0900

    impl: working entry management

--- entry/index.mjs
+++ entry/index.mjs
@@ -1,16 +1,212 @@
 import fs from 'fs';
+import path from 'path';
 import express from 'express'
 import pinoHttp from 'pino-http';
+import { JSONFilePreset } from 'lowdb/node';
+
+const db = await JSONFilePreset(path.join(path.resolve(), 'db.json'), {});
+const web = path.join(path.resolve(), 'web');
 
 const app = express();
 
+app.use(express.json());
+app.use(express.static(web));
 app.use(express.urlencoded({ extended: true }));
 app.use(pinoHttp({ stream: fs.createWriteStream('./app.log', { flags: 'a' }) }));
 
 app.listen(5000);
 
-app.get('/', (req, res) => {
-  req.log.info({ custom: 'test' }, 'response');
-  res.send('Hello World');
+// return all entries
+app.get('/all', async (req, res) => {
+  if (req.query.download !== undefined) {
+    res.setHeader('Content-Disposition', 'attachment; filename="entry.json"');
+    res.setHeader('Content-Type', 'application/json');
+
+    await db.read();
+    res.send(JSON.stringify(db.data, null, 2));
+  } else {
+    await db.read();
+    res.json(db.data);
+  }
 });
 
+// return specific entry by number
+app.get('//team', async (req, res) => {
+  if (req.query.num === undefined) {
+    return res.status(400).send('Err: missing entry number');
+  }
+
+  await db.read();
+
+  if (db.data[req.query.num] === undefined) {
+    return res.status(400).send(`Err: entry ${req.query.num} not exists`);
+  } else {
+    return res.json(db.data[req.query.num]);
+  }
+});
+
+// add new entry
+app.post('//team', async (req, res) => {
+  let num = Number(req.body.num);
+
+  if (req.body.num === '' || Number.isNaN(num) || num < 0) {
+    return res.status(400).send('Err: bad entry number');
+  }
+
+  if (req.body.univ === undefined || req.body.univ.trim() === '') {
+    return res.status(400).send('Err: bad entry univ');
+  }
+
+  if (req.body.team === undefined || req.body.team.trim() === '') {
+    return res.status(400).send('Err: bad entry team');
+  }
+
+  await db.read();
+
+  if (db.data[num]) {
+    return res.status(400).send(`Err: entry ${num} already exists`);
+  }
+
+  db.data[num] = {
+    univ: req.body.univ.trim(),
+    team: req.body.team.trim(),
+  };
+
+  await db.write();
+  res.status(201).send();
+});
+
+// update entry
+app.patch('//team', async (req, res) => {
+  let num = Number(req.body.num);
+
+  if (req.body.num === '' || Number.isNaN(num) || num < 0) {
+    return res.status(400).send('Err: bad entry number');
+  }
+
+  if (req.body.univ === undefined || req.body.univ.trim() === '') {
+    return res.status(400).send('Err: bad entry univ');
+  }
+
+  if (req.body.team === undefined || req.body.team.trim() === '') {
+    return res.status(400).send('Err: bad entry team');
+  }
+
+  await db.read();
+
+  if (req.body.num_changed === false) {
+    if (db.data[num] === undefined) {
+      return res.status(400).send(`Err: entry ${num} not exists`);
+    }
+
+    db.data[num] = {
+      univ: req.body.univ.trim(),
+      team: req.body.team.trim(),
+    };
+  } else {
+    let prev = Number(req.body.prev);
+
+    if (req.body.prev === '' || Number.isNaN(prev) || prev < 0) {
+      return res.status(400).send('Err: bad prev entry number');
+    }
+
+    if (db.data[prev] === undefined) {
+      return res.status(400).send(`Err: entry ${prev} not exists`);
+    }
+
+    if (db.data[num]) {
+      return res.status(400).send(`Err: entry ${num} already exists`);
+    }
+
+    db.data[num] = db.data[prev];
+    delete db.data[prev];
+  }
+
+  await db.write();
+  res.status(200).send();
+});
+
+// remove entry
+app.delete('//team', async (req, res) => {
+  let num = Number(req.body.num);
+
+  if (req.body.num === '' || Number.isNaN(num) || num < 0) {
+    return res.status(400).send('Err: bad entry number');
+  }
+
+  await db.read();
+
+  if (db.data[num] === undefined) {
+    return res.status(400).send(`Err: entry ${num} not exists`);
+  }
+
+  delete db.data[num];
+  await db.write();
+  res.status(200).send();
+});
+
+// replace db
+app.post('//upload', async (req, res) => {
+  let data;
+
+  try {
+    data = JSON.parse(req.body.data);
+  } catch (e) {
+    return res.status(400).send(`Err: bad db format: ${e}`);
+  }
+
+  if (validate(data)) {
+    db.data = data;
+    await db.write();
+  } else {
+    return res.status(400).send('Err: bad db format');
+  }
+
+  res.status(200).send();
+});
+
+// serve static files
+app.get('/', async (req, res) => {
+  try {
+    if (req.path === '/') {
+      return res.sendFile(path.join(web, 'index.html'));
+    }
+
+    const resource = path.join(web, req.path);
+    await fs.access(resource);
+
+    res.sendFile(resource, e => {
+      if (e) {
+        res.status(500).send(e);
+      }
+    });
+  } catch (e) {
+    res.status(404).send(e);
+  }
+});
+
+function validate(data) {
+  if (data === undefined || data === null || typeof data !== 'object') {
+    return false;
+  }
+
+  for (let key in data) {
+    if (!/^\d+$/.test(key)) {
+      return false;
+    }
+
+    const value = data[key];
+
+    if (typeof value !== 'object' || value === null) {
+      return false;
+    }
+
+    const keys = Object.keys(value);
+
+    if (keys.length !== 2 || !keys.includes('univ') || !keys.includes('team')) {
+      return false;
+    }
+  }
+
+  return true;
+}
---
+++ entry/web/script.js
@@ -0,0 +1,173 @@
+let notyf = new Notyf({
+  ripple: false,
+  duration: 3500,
+  types: [{
+    type: 'warn',
+    background: 'orange',
+    icon: {
+      className: 'fa fa-exclamation-triangle',
+      tagName: 'i',
+      color: 'white',
+    }
+  }]
+});
+
+let table = new simpleDatatables.DataTable("#entry-table", {
+  columns: [
+    { select: 0, sort: "asc" },
+    { select: 1 },
+    { select: 2 },
+    {
+      select: 3, sortable: false, type: "string", render: (value, td, row, cell) => {
+        let entry = Number(table.data.data[row].cells[0].text);
+        return `<span class="remove btn red small" onclick="remove(${entry})"><i class="fa fw fa-delete-left"></i></span>`;
+      }
+    },
+  ],
+  data: {
+    headings: [
+      { text: "엔트리", data: "num" },
+      { text: "학교", data: "univ" },
+      { text: "팀", data: "team" },
+      { text: "삭제", data: "del" }
+    ],
+  },
+  perPage: 100,
+  perPageSelect: [10, 20, 50, 100],
+});
+
+simpleDatatables.makeEditable(table, { contextMenu: false });
+
+table.on("editable.save.cell", (newValue, oldValue, row, column) => {
+  if (newValue === oldValue) {
+    return;
+  }
+
+  edit({
+    num: table.data.data[row].cells[0].text,
+    univ: table.data.data[row].cells[1].text,
+    team: table.data.data[row].cells[2].text,
+    num_changed: column === 0,
+    prev: oldValue,
+  });
+});
+
+/* prevent editor doubleclick event for the delete entry buttons **************/
+document.getElementById("entry-table").addEventListener("dblclick", e => {
+  if (e.target.classList.contains('remove') || e.target.querySelector(".remove")) {
+    e.stopImmediatePropagation();
+  }
+});
+
+update();
+
+/* functions ******************************************************************/
+async function update() {
+  try {
+    const res = await fetch('/entry/all');
+
+    if (!res.ok) {
+      throw new Error(`failed to get entry list: ${res.status}`);
+    }
+
+    let result = Object.entries(await res.json()).map(([key, value]) => ({
+      num: Number(key), ...value
+    }));
+
+    table.data.data = [];
+    table.update();
+    table.insert(result.map(x => { x.del = ""; return x }));
+  } catch (e) {
+    notyf.error(e.message);
+  }
+}
+
+async function add() {
+  let num = document.getElementById("new-num").value;
+  let univ = document.getElementById("new-univ").value;
+  let team = document.getElementById("new-team").value;
+
+  try {
+    const res = await fetch('/entry/team', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ num, univ, team })
+    });
+
+    if (!res.ok) {
+      throw new Error(`add failed: ${await res.text()}`);
+    }
+
+    document.getElementById("new-num").value = '';
+    document.getElementById("new-univ").value = '';
+    document.getElementById("new-team").value = '';
+
+    notyf.success(`${num}번 엔트리를 추가했습니다.`);
+    update();
+  } catch (e) {
+    notyf.error(e.message);
+  }
+}
+
+async function edit(entry) {
+  try {
+    const res = await fetch(`/entry/team`, {
+      method: 'PATCH',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(entry)
+    });
+
+    if (!res.ok) {
+      throw new Error(`edit failed: ${await res.text()}`);
+    }
+
+    notyf.success(`${entry.num}번 엔트리를 수정했습니다.`);
+    update();
+  } catch (e) {
+    notyf.error(e.message);
+  }
+}
+
+async function remove(num) {
+  try {
+    const res = await fetch(`/entry/team`, {
+      method: 'DELETE',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ num })
+    });
+
+    if (!res.ok) {
+      throw new Error(`remove failed: ${await res.text()}`);
+    }
+
+    notyf.success(`${num}번 엔트리를 삭제했습니다.`);
+    update();
+  } catch (e) {
+    notyf.error(e.message);
+  }
+}
+
+async function upload() {
+  const file = document.getElementById('file').files[0];
+
+  if (!file) {
+    return notyf.error('파일을 선택하세요.');
+  }
+
+  try {
+    const res = await fetch('/entry/upload', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ data: await file.text() })
+    });
+
+    if (!res.ok) {
+      throw new Error(`upload failed: ${await res.text()}`);
+    }
+
+    notyf.success('파일 업로드 완료');
+    update();
+  } catch (e) {
+    notyf.error(e.message);
+  }
+}
